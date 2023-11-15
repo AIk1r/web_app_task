000f536c268b9b95a6fcebe30e2d9c74ff4cdf8d
Alkir <111125825+AIk1r@users.noreply.github.com>
2023-11-15 12:26:07 +0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,33 @@
+from fastapi.testclient import TestClient
+from .app import app
+
+client = TestClient(app)
+
+def test_create_form():
+   response = client.post(
+       "/create_form",
+       json={
+           "name": "Test Form",
+           "fields": {
+               "phone": "+7 123 456 78 90",
+               "email": "test@example.com"
+           }
+       }
+   )
+   assert response.status_code == 200
+   assert response.json() == {"success": "Form saved successfully"}
+
+def test_get_form():
+   # Добавьте тестовую форму в базу данных перед этим тестом
+   response = client.post(
+       "/get_form",
+       json={
+           "name": "Test Form",
+           "fields": {
+               "phone": "+7 123 456 78 90",
+               "email": "test@example.com"
+           }
+       }
+   )
+   assert response.status_code == 200
+   assert response.json() == {"phone": "+7 123 456 78 90", "email": "test@example.com"}
